45ca497a32f57708b90c1c9ec55266c5bc39ce95
Quintin_Umi <qqj030212@gmail.com>
2022-11-05 20:37:54 +0800

    Structure built-up

---
+++ Assignment2/A2_for_develop_n_debug.py
@@ -0,0 +1,10 @@
+#####################################################################################
+#   This is for programming develop and debug only, but not for the final product   
+#                                                                                   
+#####################################################################################
+
+import phonebook_package.phonebook as pb
+
+print(pb.phoneRec.a)
+pb.phoneRec.a = 6
+print(pb.phoneRec.a)
---
+++ Assignment2/phonebook_package/phonebook.py
@@ -0,0 +1,17 @@
+#####################################################################################
+#   This module is developed by ENG2002-Group7                                   
+#   
+#   Contributers for tasks:
+#       Task 0: 
+#       Task 1:
+#       Task 2:
+#       Task 3:
+#       Task 4:                                                                          
+#####################################################################################
+
+class phoneRec:
+    a = 5
+
+class phoneBk:
+    pass
+
